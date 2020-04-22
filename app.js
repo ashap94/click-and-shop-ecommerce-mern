f4371439a16702d9b187121c9331afc52bfa73c4
ashap94 <aaronshapiro94@gmail.com>
2020-04-22 16:07:17 -0700

    First Commit
    downloaded packages, express, mongoose, dotenv
    setup connection with mongodb atlas cloud
    setup some user routes and defined controller methods

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// import routes
+const userRoutes = require("./routes/user");
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err}`);
+});
+
+// routes middleware
+
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
