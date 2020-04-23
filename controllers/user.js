0d60226efd795a51787384443a634447109b355e
ashap94 <aaronshapiro94@gmail.com>
2020-04-23 14:44:28 -0700

    Added isAuth & isAdmin middlewares that detects if a user is authenticated via the JWT they send up to the backend
    if the params userId matches the id in the JWT payload so that they may access that paricular route
    and if the user is an Admin to access a particular route

--- controllers/user.js
+++ controllers/user.js
@@ -1,59 +1,17 @@
 const User = require("../models/user");
-const jwt = require("jsonwebtoken");
-const expressJwt = require("express-jwt");
-const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.signup = (req, res) => {
-  console.log("req.body: ", req.body);
-  const user = new User(req.body);
-
-  user.save((error, user) => {
-    if (error) {
-      res.status(400).json({
-        error: errorHandler(error),
+exports.userById = (req, res, next, id) => {
+  User.findById(id).exec((error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error,
       });
     }
 
     user.salt = undefined;
     user.hashed_password = undefined;
 
-    res.json({
-      user,
-    });
-  });
-};
-
-exports.signin = (req, res) => {
-  // find the user based on email
-  const { email, password } = req.body;
-  User.findOne({ email }, (err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        error: "User with that email does not exist. Please signup.",
-      });
-    }
-    // if user is found make sure the email and password match
-    // create authenticate method in user model
-    if (!user.authenticate(password)) {
-      return res.status(401).json({
-        error: "Email and Password don't match.",
-      });
-    }
-    // generate a signed token with user id and secret
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    // persist the token as 't' in cookie with expiry date
-    res.cookie("t", token, {
-      expire: new Date() + 9999,
-    });
-    // return response with user and token to frontend client
-    const { _id, email, name, role } = user;
-    return res.json({ token, user: { _id, email, name, role } });
-  });
-};
-
-exports.signout = (req, res) => {
-  res.clearCookie("t");
-  res.json({
-    message: "Signout Success",
+    req.profile = user;
+    next();
   });
 };
